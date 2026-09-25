56a023540ba8b2b7663e38cf18c363de7c647dd2
chrischall <chris.c.hall@gmail.com>
2026-09-25 11:48:51 -0400

    chore(main): release 3.2.1 (#170)
    :robot: I have created a release *beep* *boop*
    ---
    
    
    ##
    [3.2.1](https://github.com/chrischall/ioffice-mcp/compare/v3.2.0...v3.2.1)
    (2026-09-24)
    
    
    ### Bug Fixes
    
    * **deps:** bump dotenv from 17.4.2 to 18.0.2 in the production-majors
    group ([#169](https://github.com/chrischall/ioffice-mcp/issues/169))
    ([6a99103](https://github.com/chrischall/ioffice-mcp/commit/6a9910325d58541f7dfcb827c682e36608fc64e8))
    
    ---
    This PR was generated with [Release
    Please](https://github.com/googleapis/release-please). See
    [documentation](https://github.com/googleapis/release-please#release-please).

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ const client = new IOfficeClient();
 
 await runMcp({
   name: 'ioffice',
-  version: '3.2.0', // x-release-please-version
+  version: '3.2.1', // x-release-please-version
   deps: client,
   tools: [
     registerBuildingTools,
